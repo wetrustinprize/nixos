fc92ce0adb4f7cabadf24d4570b6d89911c8907d
Peterson Adami Candido <me@wetrustinprize.com>
2024-02-13 19:14:59 -0300

    feat: barrier on Dionysus

--- users/wetrustinprize/dionysus.nix
+++ users/wetrustinprize/dionysus.nix
@@ -3,6 +3,12 @@
 {
   home.packages = with pkgs; [ unityhub gimp ];
 
+  services.barrier.client = {
+    enable = true;
+    name = "dionysus";
+    serverAddress = "192.168.18.2"
+  };
+
   services.polybar.script = ''
     killall -q polybar
     while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
