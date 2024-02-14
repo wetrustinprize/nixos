1e824089508b29480338334bdb74e57a772ca760
Prize <me@wetrustinprize.com>
2024-02-14 05:44:44 +0000

    feat: home-manager in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,42 +1,42 @@
 {
-
-  description = "My first flake!";
-
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11";
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager, ... }:
     let
-      lib = nixpkgs.lib;
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      makeHome = { username, hostname }:
-        home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = builtins.filter (x: x != null) [
-            ./users/${username}/home.nix
-            (if builtins.pathExists ./users/${username}/${hostname}.nix then
-              ./users/${username}/${hostname}.nix
-            else
-              null)
+      lib = nixpkgs.lib;
+      mkConfig = { hostname, username ? "wetrustinprize", system }:
+        lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./hosts/${hostname}/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.users.${username} = {
+                imports = builtins.filter (x: x != null) [
+                  ./users/${username}/home.nix
+                  (if builtins.pathExists
+                  ./users/${username}/${hostname}.nix then
+                    ./users/${username}/${hostname}.nix
+                  else
+                    null)
+                ];
+              };
+            }
           ];
         };
     in {
       nixosConfigurations = {
-        dionysus = lib.nixosSystem {
+        dionysus = mkConfig {
           inherit system;
-          modules = [ ./hosts/dionysus/configuration.nix ];
-        };
-      };
-      homeConfigurations = {
-        "wetrustinprize@dionysus" = makeHome {
-          username = "wetrustinprize";
           hostname = "dionysus";
         };
       };
     };
-
 }
