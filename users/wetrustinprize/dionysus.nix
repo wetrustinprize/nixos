3d3caa9c6b9132302c4fc25252539bc0dff180e9
Prize <me@wetrustinprize.com>
2024-02-14 05:44:33 +0000

    feat: barrier

--- users/wetrustinprize/dionysus.nix
+++ users/wetrustinprize/dionysus.nix
@@ -1,12 +1,7 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ unityhub gimp ];
-
-  services.barrier.client = {
-    enable = true;
-    name = "dionysus";
-  };
+  home.packages = with pkgs; [ unityhub gimp barrier ];
 
   services.polybar.script = ''
     killall -q polybar
