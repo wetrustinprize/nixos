a1caf799b3f62744ae70b1d4b3e51ae42031d220
Prize <me@wetrustinprize.com>
2024-02-17 05:15:15 +0000

    feat: mini lightdm greeter

--- hosts/desktop.nix
+++ hosts/desktop.nix
@@ -20,7 +20,35 @@
   services.xserver = {
     enable = true;
     displayManager = {
-      gdm.enable = true;
+      lightdm = {
+        enable = true;
+        greeter.enable = true;
+        greeters.mini = {
+          enable = true;
+          user = "wetrustinprize";
+          extraConfig = ''
+            [greeter]
+            show-sys-info=true
+            password-alignment = center
+
+            [greeter-theme]
+            font-weight = regular
+
+            text-color=#eceff4
+            error-color=#bf616a
+            background-image = ""
+            background-color = "#2e3440"
+
+            window-color = "#4c566a"
+            border-color = "#eceff4"
+            border-width = 1px
+
+            password-color = "#2e3440"
+            password-background-color = "#eceff4"
+            password-border-width = 0px
+          '';
+        };
+      };
       defaultSession = "none+i3";
     };
     windowManager.i3 = { enable = true; };
