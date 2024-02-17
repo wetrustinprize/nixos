c827cc92d8dc1435eb870f7e81bc2e17d290146e
Prize <me@wetrustinprize.com>
2024-02-17 07:48:12 +0000

    feat: dionysus nvidia :tada:

--- hosts/dionysus/configuration.nix
+++ hosts/dionysus/configuration.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 
 {
   imports = [ ../common.nix ../desktop.nix ./hardware-configuration.nix ];
@@ -18,4 +18,35 @@
   hardware.bluetooth.powerOnBoot = true;
 
   environment.systemPackages = with pkgs; [ bluetuith ];
+
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  nixpkgs.config.nvidia.acceptLicense = true;
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "nvidia-x11"
+      "nvidia-settings"
+      "nvidia-persistenced"
+    ];
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+    modesetting.enable = true;
+    nvidiaSettings = true;
+    open = false;
+    powerManagement.finegrained = false;
+    powerManagement.enable = false;
+  };
+
+  hardware.nvidia.prime = {
+    sync.enable = true;
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:3:0:0";
+  };
 }
