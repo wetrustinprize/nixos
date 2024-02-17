ce91fd276e644d567e4dec8da1812b86e3fbe3a5
Prize <me@wetrustinprize.com>
2024-02-17 07:07:42 +0000

    feat: pipewire :tada:

--- hosts/desktop.nix
+++ hosts/desktop.nix
@@ -56,6 +56,13 @@
 
   programs.nix-ld.enable = true;
 
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [ pulseaudioFull ];
 }
