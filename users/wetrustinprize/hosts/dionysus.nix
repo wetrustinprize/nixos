2898188e88c5df6a3c3e8396d0b4e9393bab5f5c
Prize <me@wetrustinprize.com>
2024-02-18 06:45:08 +0000

    fix: battery polybar on dionysus

--- users/wetrustinprize/hosts/dionysus.nix
+++ users/wetrustinprize/hosts/dionysus.nix
@@ -14,6 +14,10 @@
 
   services.polybar.settings."bar/primary".modules-right = lib.mkForce
     "pulseaudio divider mute-mic mute-dunst divider battery divider open date time close";
+  services.polybar.settings."module/battery" = {
+    battery = "BAT1";
+    adapter = "ADP1";
+  };
 
   services.dunst.settings.global.font = lib.mkForce "Jetbrain 14";
 }
