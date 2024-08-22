50b6590005c1728de0695d144de792f5e053e2d6
Peterson Adami Candido <me@wetrustinprize.com>
2024-08-22 01:24:15 -0300

    fix

--- hosts/dionysus/configuration.nix
+++ hosts/dionysus/configuration.nix
@@ -24,6 +24,7 @@
   time.timeZone = "America/SaoPaulo";
 
   services.xserver.xkb.layout = "br";
+  services.logind.lidSwitch = "nothing";
 
   powerManagement.enable = true;
   services.thermald.enable = true;
