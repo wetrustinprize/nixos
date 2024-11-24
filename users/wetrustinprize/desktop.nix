b3d2666da50d1bc31e0a9dc5c07ae853f15b28c9
Peterson Adami Candido <me@wetrustinprize.com>
2024-11-24 00:28:17 -0300

    fix

--- users/wetrustinprize/desktop.nix
+++ users/wetrustinprize/desktop.nix
@@ -2,11 +2,11 @@
 
 {
   imports = [
-    ../modules/i3/i3.nix
-    ../modules/polybar/polybar.nix
-    ../modules/alacritty.nix
-    ../modules/dunst.nix
-    ../modules/rofi.nix
-    ../modules/chromium.nix
+    ./modules/i3/i3.nix
+    ./modules/polybar/polybar.nix
+    ./modules/alacritty.nix
+    ./modules/dunst.nix
+    ./modules/rofi.nix
+    ./modules/chromium.nix
   ];
 }
