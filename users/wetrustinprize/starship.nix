c610db294b534e6e81b51dfb2ddb02b52ef79037
wetrustinprize <me@wetrustinprize.com>
2025-02-22 01:09:43 -0300

    format

--- users/wetrustinprize/starship.nix
+++ users/wetrustinprize/starship.nix
@@ -1,21 +1,22 @@
-{config, ...}: {
-	programs.starship = {
-		enable = true;
-		enableBashIntegration = true;
-		enableNushellIntegration = true;
+{ config, ... }:
+{
+  programs.starship = {
+    enable = true;
+    enableBashIntegration = true;
+    enableNushellIntegration = true;
 
-		settings = {
-			palette = "nix-colors";
-			palettes.nix-colors = {
-				black = "#${config.colorScheme.palette.base00}";
-				red = "#${config.colorScheme.palette.base08}";
-				green = "#${config.colorScheme.palette.base0B}";
-				blue = "#${config.colorScheme.palette.base0D}";
-				yellow = "#${config.colorScheme.palette.base0A}";
-				purple = "#${config.colorScheme.palette.base0E}";
-				cyan = "#${config.colorScheme.palette.base0C}";
-				white = "#${config.colorScheme.palette.base05}";
-			};
-		};
-	};
+    settings = {
+      palette = "nix-colors";
+      palettes.nix-colors = {
+        black = "#${config.colorScheme.palette.base00}";
+        red = "#${config.colorScheme.palette.base08}";
+        green = "#${config.colorScheme.palette.base0B}";
+        blue = "#${config.colorScheme.palette.base0D}";
+        yellow = "#${config.colorScheme.palette.base0A}";
+        purple = "#${config.colorScheme.palette.base0E}";
+        cyan = "#${config.colorScheme.palette.base0C}";
+        white = "#${config.colorScheme.palette.base05}";
+      };
+    };
+  };
 }
