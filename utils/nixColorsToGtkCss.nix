34d068bafc81684c5f616d44a810afa5cf24e8d6
wetrustinprize <me@wetrustinprize.com>
2025-03-04 10:41:54 -0300

    feat: gtk css utils

---
+++ utils/nixColorsToGtkCss.nix
@@ -0,0 +1,19 @@
+{ lib, ... }:
+	scheme: ''
+		@define-color base-00 ${scheme.palette.base00};
+		@define-color base-01 ${scheme.palette.base01};
+		@define-color base-02 ${scheme.palette.base02};
+		@define-color base-03 ${scheme.palette.base03};
+		@define-color base-04 ${scheme.palette.base04};
+		@define-color base-05 ${scheme.palette.base05};
+		@define-color base-06 ${scheme.palette.base06};
+		@define-color base-07 ${scheme.palette.base07};
+		@define-color base-08 ${scheme.palette.base08};
+		@define-color base-09 ${scheme.palette.base09};
+		@define-color base-0A ${scheme.palette.base0A};
+		@define-color base-0B ${scheme.palette.base0B};
+		@define-color base-0C ${scheme.palette.base0C};
+		@define-color base-0D ${scheme.palette.base0D};
+		@define-color base-0E ${scheme.palette.base0E};
+		@define-color base-0F ${scheme.palette.base0F};
+	''
