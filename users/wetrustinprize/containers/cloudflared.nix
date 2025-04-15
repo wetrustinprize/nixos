22a1548171accaf1e0fc1fd5d359b6abd8e3d903
wetrustinprize <me@wetrustinprize.com>
2025-04-15 12:30:26 -0300

    fix: cloudflared

--- users/wetrustinprize/containers/cloudflared.nix
+++ users/wetrustinprize/containers/cloudflared.nix
@@ -2,17 +2,12 @@
 
 let
   hasSecret = lib.hasAttr "cloudflared" config.sops.secrets;
+  _ = lib.warnIf (!hasSecret) "⚠️ Secret 'cloudflared' not found — skipping cloudflared container.";
+
   token = builtins.readFile config.sops.secrets.cloudflared.path;
 in
 {
   config = {
-    assertions = [
-      {
-        assertion = hasSecret;
-        message = "Secret 'cloudflared' not found — skipping cloudflared container.";
-      }
-    ];
-
     services.podman.containers = lib.mkIf hasSecret {
       "cloudflared" = {
         image = "cloudflare/cloudflared:latest";
