517af6f23690140a6dc250f95c28c4e4955da8b4
wetrustinprize <me@wetrustinprize.com>
2025-04-15 12:27:59 -0300

    fix: cloudflared

--- users/wetrustinprize/containers/cloudflared.nix
+++ users/wetrustinprize/containers/cloudflared.nix
@@ -1,24 +1,27 @@
 { lib, config, ... }:
+
 let
   hasSecret = lib.hasAttr "cloudflared" config.sops.secrets;
-  token = lib.readFile config.sops.secrets.cloudflared.path;
+  token = builtins.readFile config.sops.secrets.cloudflared.path;
 in
 {
-  config = lib.mkIf hasSecret {
-    services.podman.containers."cloudflared" = {
-      image = "cloudflare/cloudflared:latest";
-      autoStart = true;
-      autoUpdate = "local";
-      environment = {
-        TUNNEL_TOKEN = token;
+  config = {
+    assertions = [
+      {
+        assertion = hasSecret;
+        message = "Secret 'cloudflared' not found — skipping cloudflared container.";
+      }
+    ];
+
+    services.podman.containers = lib.mkIf hasSecret {
+      "cloudflared" = {
+        image = "cloudflare/cloudflared:latest";
+        autoStart = true;
+        autoUpdate = "local";
+        environment = {
+          TUNNEL_TOKEN = token;
+        };
       };
     };
   };
-
-  assertions = [
-    {
-      assertion = hasSecret;
-      message = "Secret 'cloudflared' not found — skipping cloudflared container.";
-    }
-  ];
 }
