a6f83d5093a86ae0ee28a6a4af4d06cdf0645335
wetrustinprize <me@wetrustinprize.com>
2025-04-16 18:54:18 -0300

    fix: entrypoints

--- hosts/atena/containers/actual.nix
+++ hosts/atena/containers/actual.nix
@@ -5,7 +5,7 @@
         labels = {
             "traefik.enable" = "true";
             "traefik.http.routers.actual.rule" = "Path(`/actual`)";
-            "traefik.http.routers.actual.entrypoints" = "web";
+            "traefik.http.routers.actual.entrypoints" = "http";
             "traefik.http.routers.actual.middlewares" = "actual-strip";
             "traefik.http.middlewares.actual-strip.stripprefix.prefixes" = "/actual";
             "traefik.http.services.actual.loadbalancer.server.port" = "5006";
