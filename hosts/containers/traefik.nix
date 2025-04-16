2b576afe930109663d8b20a5182566ebc3cbeed5
wetrustinprize <me@wetrustinprize.com>
2025-04-16 18:51:35 -0300

    fix: docker socket

--- hosts/containers/traefik.nix
+++ hosts/containers/traefik.nix
@@ -3,6 +3,9 @@
         image = "traefik:v3.3";
         autoStart = true;
         cmd = ["--api.insecure=true" "--providers.docker"];
+        volumes = [
+            "/var/run/docker.sock:/var/run/docker.sock"
+        ];
         ports = [
             "80:80"
             "8080:8080"
