20ae15b832600030f16afbc02324d4e1f97cb54c
wetrustinprize <me@wetrustinprize.com>
2025-04-18 09:03:31 -0300

    fix: styling

--- hosts/atena/containers/actual.nix
+++ hosts/atena/containers/actual.nix
@@ -1,17 +1,18 @@
-{ ... }: {
-    virtualisation.oci-containers.containers."actual" = {
-        image = "actualbudget/actual-server:latest";
-        autoStart = true;
-        labels = {
-            "traefik.enable" = "true";
-            "traefik.http.routers.actual.rule" = "Path(`/actual`)";
-            "traefik.http.routers.actual.entrypoints" = "http";
-            "traefik.http.routers.actual.middlewares" = "actual-strip";
-            "traefik.http.middlewares.actual-strip.stripprefix.prefixes" = "/actual";
-            "traefik.http.services.actual.loadbalancer.server.port" = "5006";
-        };
-        volumes = [
-            "/srv/actual:/data"
-        ];
+{ ... }:
+{
+  virtualisation.oci-containers.containers."actual" = {
+    image = "actualbudget/actual-server:latest";
+    autoStart = true;
+    labels = {
+      "traefik.enable" = "true";
+      "traefik.http.routers.actual.rule" = "Path(`/actual`)";
+      "traefik.http.routers.actual.entrypoints" = "http";
+      "traefik.http.routers.actual.middlewares" = "actual-strip";
+      "traefik.http.middlewares.actual-strip.stripprefix.prefixes" = "/actual";
+      "traefik.http.services.actual.loadbalancer.server.port" = "5006";
     };
+    volumes = [
+      "/srv/actual:/data"
+    ];
+  };
 }
--- hosts/containers/traefik.nix
+++ hosts/containers/traefik.nix
@@ -1,14 +1,18 @@
-{ ... }: {
-    virtualisation.oci-containers.containers."traefik" = {
-        image = "traefik:v3.3";
-        autoStart = true;
-        cmd = ["--api.insecure=true" "--providers.docker"];
-        volumes = [
-            "/var/run/docker.sock:/var/run/docker.sock"
-        ];
-        ports = [
-            "80:80"
-            "8080:8080"
-        ];
-    };
+{ ... }:
+{
+  virtualisation.oci-containers.containers."traefik" = {
+    image = "traefik:v3.3";
+    autoStart = true;
+    cmd = [
+      "--api.insecure=true"
+      "--providers.docker"
+    ];
+    volumes = [
+      "/var/run/docker.sock:/var/run/docker.sock"
+    ];
+    ports = [
+      "80:80"
+      "8080:8080"
+    ];
+  };
 }
