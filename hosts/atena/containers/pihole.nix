e96f0c48a8c51defd4a370b659ce54caeca6e85b
wetrustinprize <me@wetrustinprize.com>
2025-04-18 09:40:54 -0300

    fix: traefik typos

--- hosts/atena/containers/pihole.nix
+++ hosts/atena/containers/pihole.nix
@@ -13,13 +13,13 @@
             # FIXME: Make this more secure
             "FTLCONF_webserver_api_password" = "pihole"; 
         };
-        labels = [
-            "label=traefik.enable" = "true"
-            "label=traefik.http.routers.pihole.rule" = "Path(`/pihole`)"
-            "label=traefik.http.routers.pihole.entrypoints" = "websecure"
-            "label=traefik.http.routers.pihole.tls" = "true"
-            "label=traefik.http.services.pihole.loadbalancer.server.port" = "80"
-        ];
+        labels = {
+            "label=traefik.enable" = "true";
+            "label=traefik.http.routers.pihole.rule" = "Path(`/pihole`)";
+            "label=traefik.http.routers.pihole.entrypoints" = "websecure";
+            "label=traefik.http.routers.pihole.tls" = "true";
+            "label=traefik.http.services.pihole.loadbalancer.server.port" = "80";
+        };
         volumes = [
             "/srv/pihole:/etc/pihole"
         ];
