46975367b8d58941e7276f2b572b64f0553339f5
wetrustinprize <me@wetrustinprize.com>
2025-04-18 09:51:21 -0300

    fix: changed pihole dns port

---
+++ hosts/atena/containers/matrix.nix
@@ -0,0 +1,9 @@
+{ ... }: {
+    virtualisation.oci-containers.containers = {
+        "matrix-synapse" = {
+            image = "matrixdotorg/synapse:latest";
+            autoStart = true;
+            cmd = [ "generate" ];
+        };
+    };
+}
