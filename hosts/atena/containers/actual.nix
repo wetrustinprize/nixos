0b672962fb47580868bc1d4bc44f56db04aa3de1
wetrustinprize <me@wetrustinprize.com>
2025-04-20 18:29:19 -0300

    fix: cloudflare as tls provider

--- hosts/atena/containers/actual.nix
+++ hosts/atena/containers/actual.nix
@@ -7,7 +7,7 @@
       "traefik.enable" = "true";
       "traefik.http.routers.actual.rule" = "Host(`actual.wetrustinprize.com`) || Host(`actual.home.wetrustinprize.com`)";
       "traefik.http.routers.actual.tls" = "true";
-      "traefik.http.routers.actual.tls.certresolver" = "digitalocean";
+      "traefik.http.routers.actual.tls.certresolver" = "cloudflare";
       "traefik.http.routers.actual.entrypoints" = "websecure";
       "traefik.http.services.actual.loadbalancer.server.port" = "5006";
     };
