b4842a72f2e61f08806b9f7aa57f453e456a0a0e
wetrustinprize <me@wetrustinprize.com>
2025-04-20 23:44:02 -0300

    feat: softwarr services

--- hosts/atena/containers/softwarr/softwarr.nix
+++ hosts/atena/containers/softwarr/softwarr.nix
@@ -1,9 +1,11 @@
 { ... }:
 let
-    genTraefikLabels = { service, port }: {
+  genTraefikLabels =
+    { service, port }:
+    {
       "traefik.enable" = "true";
       "traefik.http.routers.${service}.rule" =
-        "Host(`softwarr.home.wetrustinprize.com`) && PathPrefix(`${service}`)";
+        "Host(`softwarr.home.wetrustinprize.com`) && PathPrefix(`/${service}`)";
       "traefik.http.routers.${service}.entrypoints" = "web";
       "traefik.http.routers.${service}.middlewares" = "strip-${service}";
       "traefik.http.middlewares.strip-${service}.stripprefix.prefixes" = "/${service}";
@@ -11,11 +13,51 @@ let
     };
 in
 {
-    virtualisation.oci-containers.containers = {
-        "prowlarr" = {
-            image = "lscr.io/linuxserver/prowlarr:latest";
-            autoStart = true;
-            labels = genTraefikLabels { service="prowlarr"; port=9696; };
-        };
+  virtualisation.oci-containers.containers = {
+    "prowlarr" = {
+      image = "lscr.io/linuxserver/prowlarr:latest";
+      autoStart = true;
+      labels = genTraefikLabels {
+        service = "prowlarr";
+        port = 9696;
+      };
     };
+    "radarr" = {
+      image = "lscr.io/linuxserver/radarr:latest";
+      autoStart = true;
+      labels = genTraefikLabels {
+        service = "radarr";
+        port = 7878;
+      };
+    };
+    "sonarr" = {
+      image = "lscr.io/linuxserver/sonnar:latest";
+      autoStart = true;
+      labels = genTraefikLabels {
+        service = "sonnar";
+        port = 8989;
+      };
+    };
+    "bazarr" = {
+      image = "lscr.io/linuxserver/bazarr:latest";
+      autoStart = true;
+      labels = genTraefikLabels {
+        service = "bazarr";
+        port = 6767;
+      };
+    };
+    "overseerr" = {
+      image = "lscr.io/linuxserver/overseerr:latest";
+      autoStart = true;
+      labels = {
+        "traefik.enable" = "true";
+        "traefik.http.routers.overseerr.rule" =
+          "Host(`overseerr.wetrustinprize.com`) || Host(`overseerr.home.wetrustinprize.com`)";
+        "traefik.http.routers.overseerr.tls" = "true";
+        "traefik.http.routers.overseerr.tls.certresolver" = "cloudflare";
+        "traefik.http.routers.overseerr.entrypoints" = "websecure";
+        "traefik.http.services.overseerr.loadbalancer.server.port" = "5006";
+      };
+    };
+  };
 }
