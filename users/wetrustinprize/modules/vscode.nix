de9d1fdbbfdcbfe5b8c3e75fda860a3dd3b685e6
wetrustinprize <me@wetrustinprize.com>
2025-04-23 16:21:08 -0300

    feat: editor config

--- users/wetrustinprize/modules/vscode.nix
+++ users/wetrustinprize/modules/vscode.nix
@@ -58,6 +58,7 @@ in
         ms-vsliveshare.vsliveshare
         aaron-bond.better-comments
         ms-vscode-remote.remote-ssh
+        vscode-extension-editorconfig-editorconfig
 
         # toml
         tamasfe.even-better-toml
