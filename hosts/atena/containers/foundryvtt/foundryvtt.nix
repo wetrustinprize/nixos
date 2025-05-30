7ca61d5756a35fc2ee1682518d73f22e0e895566
wetrustinprize <me@wetrustinprize.com>
2025-05-30 14:27:12 -0300

    fix: not worked lol

--- hosts/atena/containers/foundryvtt/foundryvtt.nix
+++ hosts/atena/containers/foundryvtt/foundryvtt.nix
@@ -1,43 +1,43 @@
 { config, ... }:
 {
-    sops.secrets.foundryvtt = {
-        format = "dotenv";
-        sopsFile = ./.env;
-    };
+  sops.secrets.foundryvtt = {
+    format = "dotenv";
+    sopsFile = ./.env;
+  };
 
-    virtualisation.oci-containers.containers."foundryvtt" = {
-        image = "felddy/foundryvtt:latest";
-        autoStart = true;
-        labels = {
-            # Traefik
-            "traefik.enable" = "true";
-            "traefik.http.routers.foundryvtt.rule" =
-                "Host(`foundryvtt.wetrustinprize.com`) || Host(`foundryvtt.home.wetrustinprize.com`)";
-            "traefik.http.routers.foundryvtt.tls" = "true";
-            "traefik.http.routers.foundryvtt.tls.certresolver" = "cloudflare";
-            "traefik.http.routers.foundryvtt.entrypoints" = "websecure";
-            "traefik.http.services.foundryvtt.loadbalancer.server.port" = "30000";
+  virtualisation.oci-containers.containers."foundryvtt" = {
+    image = "felddy/foundryvtt:latest";
+    autoStart = true;
+    labels = {
+      # Traefik
+      "traefik.enable" = "true";
+      "traefik.http.routers.foundryvtt.rule" =
+        "Host(`foundryvtt.wetrustinprize.com`) || Host(`foundryvtt.home.wetrustinprize.com`)";
+      "traefik.http.routers.foundryvtt.tls" = "true";
+      "traefik.http.routers.foundryvtt.tls.certresolver" = "cloudflare";
+      "traefik.http.routers.foundryvtt.entrypoints" = "websecure";
+      "traefik.http.services.foundryvtt.loadbalancer.server.port" = "30000";
 
-            # Homepage
-            "homepage.group" = "Tools";
-            "homepage.name" = "foundryvtt";
-            "homepage.icon" = "foundryvtt-budget.png";
-            "homepage.description" = "Finacnes organizer.";
-            "homepage.href" = "https://foundryvtt.wetrustinprize.com";
-        };
-        volumes = [
-            "/mnt/storage/foundryvtt:/data:rw"
-        ];
-        environment = {
-            "TZ" = "America/Sao_Paulo";
-            "PUID" = "1000";
-            "PGID" = "1000";
-        };
-        environmentFiles = [
-            config.sops.secrets.foundryvtt.path
-        ];
-        extraOptions = [
-            "--dns=1.1.1.1"
-        ];
+      # Homepage
+      "homepage.group" = "Tools";
+      "homepage.name" = "foundryvtt";
+      "homepage.icon" = "foundryvtt-budget.png";
+      "homepage.description" = "Finacnes organizer.";
+      "homepage.href" = "https://foundryvtt.wetrustinprize.com";
     };
+    volumes = [
+      "/mnt/storage/foundryvtt:/data:rw"
+    ];
+    environment = {
+      "TZ" = "America/Sao_Paulo";
+      "PUID" = "1000";
+      "PGID" = "1000";
+    };
+    environmentFiles = [
+      config.sops.secrets.foundryvtt.path
+    ];
+    extraOptions = [
+      "--dns=1.1.1.1"
+    ];
+  };
 }
