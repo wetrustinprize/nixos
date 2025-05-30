7c3787403670a3f2d2b750e81d86ac374c52b763
wetrustinprize <git@wetrustinprize.com>
2025-05-30 16:55:33 -0300

    feat: auto ssh

--- hosts/ssh.nix
+++ hosts/ssh.nix
@@ -1,26 +1,33 @@
-{ hostname, ... }: {
+{ hostname, username, ... }:
+{
   programs.ssh.startAgent = true;
 
   services.openssh = {
     enable = true;
     authorizedKeysInHomedir = true;
-    hostKeys = [{
-     path = "/etc/ssh/ssh_host_ed25519_key";
-     type = "ed25519";
-   }];
+    hostKeys = [
+      {
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+    ];
   };
+
+  system.activationScripts.ssh-key-chown.text = ''
+    chown -R ${username} /etc/ssh/ssh_host_ed25519*
+  '';
 
   system.activationScripts.ssh-public-keys.text = ''
     echo "Updating SSH public keys..."
 
-    if [ -d "/home/wetrustinprize/nixos" ]; then
-      if cp /etc/ssh/ssh_host_ed25519_key.pub /home/wetrustinprize/nixos/hosts/${hostname}/; then
-        echo "✓ Successfully copied SSH public key to /home/wetrustinprize/nixos/hosts/${hostname}/"
+    if [ -d "/home/${username}/nixos" ]; then
+      if cp /etc/ssh/ssh_host_ed25519_key.pub /home/${username}/nixos/hosts/${hostname}/; then
+        echo "✓ Successfully copied SSH public key"
       else
         echo "✗ Failed to copy SSH public key"
       fi
     else
-      echo "! Skipping SSH public key copy - directory /home/wetrustinprize/nixos does not exist"
+      echo "! Skipping SSH public key copy - directory /home/${username}/nixos does not exist"
     fi
   '';
 }
