47fe72baddff0593bd6e5637af2a8a5ffd507fd0
wetrustinprize <git@wetrustinprize.com>
2025-07-02 20:42:18 -0300

    feat: android features

--- hosts/poseidon/configuration.nix
+++ hosts/poseidon/configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, username, lib, ... }:
 
 {
   imports = [
@@ -13,6 +13,12 @@
   boot.loader.efi.canTouchEfiVariables = true;
   networking.hostName = "poseidon";
   time.timeZone = "America/Sao_Paulo";
+
+  environment.systemPackages = with pkgs; [
+    android-tools
+  ];
+  programs.adb.enable = true;
+  users.users.${username}.extraGroups = lib.mkAfter ["adbusers"];
 
   hardware.opentabletdriver = {
     enable = true;
