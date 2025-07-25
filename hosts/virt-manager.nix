31152167c6618f8bd2bcb207c1bbfa2975a62934
wetrustinprize <git@wetrustinprize.com>
2025-07-25 20:08:15 -0300

    feat: fds for virtualization

--- hosts/virt-manager.nix
+++ hosts/virt-manager.nix
@@ -1,7 +1,8 @@
-{ lib, username, ... }:
+{ lib, username, pkgs, ... }:
 {
   programs.virt-manager.enable = true;
   virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd.qemu.vhostUserPackages = with pkgs; [ virtiofsd ];
   virtualisation.spiceUSBRedirection.enable = true;
 
   users.users.${username}.extraGroups = lib.mkAfter [ "libvirtd" ];
