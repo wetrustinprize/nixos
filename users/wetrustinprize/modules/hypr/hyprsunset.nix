623a4bcd0fa6f4ed0e1d30532b0aac741cf563a0
wetrustinprize <git@wetrustinprize.com>
2025-07-29 16:49:45 -0300

    fix: sunset

--- users/wetrustinprize/modules/hypr/hyprsunset.nix
+++ users/wetrustinprize/modules/hypr/hyprsunset.nix
@@ -1,5 +1,26 @@
-{ ...}: {
+{ pkgs, lib, ... }: {
+  home.packages = with pkgs; [
+    hyprsunset
+  ];
+
+  wayland.windowManager.hyprland.settings.exec-once = lib.mkAfter [ "hyprsunset" ];
+
   services.hyprsunset = {
     enable = true;
+    transitions = {
+  sunrise = {
+    calendar = "*-*-* 06:00:00";
+    requests = [
+      [ "identity" "true" ]
+    ];
+  };
+  sunset = {
+    calendar = "*-*-* 20:00:00";
+    requests = [
+      [ "temperature" "5500" ]
+      [ "gamma" "0.8" ]
+    ];
+  };
+};
   };
 }
