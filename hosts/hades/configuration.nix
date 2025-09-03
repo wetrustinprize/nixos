699e17368c57fdefc68b6da39419873f7cdbdb1c
wetrustinprize <git@wetrustinprize.com>
2025-09-03 13:37:57 -0300

    feat: hades configurations

---
+++ hosts/hades/configuration.nix
@@ -0,0 +1,36 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+
+{
+  imports = [
+    ../common.nix
+    ../audio.nix
+    ../desktop.nix
+    ./hardware-configuration.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "hades";
+  networking.firewall = {
+    enable = true;
+  };
+
+  time.timeZone = "America/Sao_Paulo";
+
+  services.logind.lidSwitch = "hibernate";
+
+  powerManagement.enable = true;
+  services.thermald.enable = true;
+
+  services.xserver = {
+    xkb.layout = "br";
+  };
+
+  system.stateVersion = "25.05";
+}
