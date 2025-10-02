41716de8ef5b7f34e5e5e36e91ec35c2739ed9f3
wetrustinprize <git@wetrustinprize.com>
2025-10-02 19:11:08 -0300

    fix(hyprpaper): removed splash

--- users/wetrustinprize/modules/hypr/hyprpaper.nix
+++ users/wetrustinprize/modules/hypr/hyprpaper.nix
@@ -1,14 +1,8 @@
 {
-  config,
   ...
 }:
 {
   services.hyprpaper = {
     enable = true;
-
-    settings = {
-      splash = true;
-      splash_color = config.colorScheme.palette.base0C;
-    };
   };
 }
