cbdcd27b003ad955134854dcece64f859a36d998
wetrustinprize <me@wetrustinprize.com>
2025-12-07 02:47:45 -0300

    feat: hades ssh configuration

--- hosts/hades/configuration.nix
+++ hosts/hades/configuration.nix
@@ -1,10 +1,21 @@
-{ inputs, ... }: {
+{ inputs, user, ... }: {
   imports = [
     ../../configuration
 
     ./hardware-configuration.nix
     inputs.nixos-hardware.nixosModules.lenovo-thinkpad-e14-amd
   ];
+
+  sops.secrets."ssh/poseidon/private" = { # ssh private key
+    owner = "${user.username}";
+    mode = "600";
+    path = "/home/${user.username}/.ssh/id_ed25519";
+  };
+  sops.secrets."ssh/poseidon/public" = { # ssh public key
+    owner = "${user.username}";
+    mode = "644";
+    path = "/home/${user.username}/.ssh/id_ed25519.pub";
+  };
 
   services.xserver.xkb = {
     layout = "br";
