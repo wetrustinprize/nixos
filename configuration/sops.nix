faa1abb1e49fc36159b4f71f3849834b767a229f
wetrustinprize <me@wetrustinprize.com>
2025-12-08 10:32:22 -0300

    fix: sops as package in system

--- configuration/sops.nix
+++ configuration/sops.nix
@@ -1,5 +1,9 @@
-{ user, ...} :
+{ user, pkgs, ...} :
 {
+  environment.systemPackages = with pkgs; [
+    sops
+  ];
+
   sops.defaultSopsFile = ../secrets/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
   sops.age.keyFile = "/home/${user.username}/.config/sops/age/keys.txt";
