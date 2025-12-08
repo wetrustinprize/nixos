cbf78f6652a47ee85c2bcab648a90e52bb28a754
wetrustinprize <me@wetrustinprize.com>
2025-12-08 10:32:26 -0300

    fix: default shell

--- configuration/shell.nix
+++ configuration/shell.nix
@@ -2,6 +2,9 @@
   console.useXkbConfig = true;
 
   environment.shells = with pkgs; [ zsh ];
+  users.defaultUserShell = pkgs.zsh;
+
+  programs.starship.enable = true;
 
   programs.zsh = {
     enable = true;
