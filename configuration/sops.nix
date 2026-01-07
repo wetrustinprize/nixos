10d3712a183c9e884a707a524c29fd62a1d0fef1
wetrustinprize <me@wetrustinprize.com>
2026-01-07 20:42:42 -0300

    fix: better formatting

--- configuration/sops.nix
+++ configuration/sops.nix
@@ -4,7 +4,9 @@
     sops
   ];
 
-  sops.defaultSopsFile = ../secrets/secrets.yaml;
-  sops.defaultSopsFormat = "yaml";
-  sops.age.keyFile = "/home/${user.username}/.config/sops/age/keys.txt";
+  sops = {
+    defaultSopsFile = ../secrets/secrets.yaml;
+    defaultSopsFormat = "yaml";
+    age.keyFile = "/home/${user.username}/.config/sops/age/keys.txt";
+  };
 }
